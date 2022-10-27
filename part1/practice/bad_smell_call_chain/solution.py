fd2faa7962ad17b022fc53de1e55a4237406eb85
s.shurupov.88@mail.ru <s.shurupov.88@mail.ru>
2022-10-27 04:12:39 +0300

    home work 22

---
+++ part1/practice/bad_smell_call_chain/solution.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self, city_population, room_num):
+        self.city_popultaion = city_population
+        self.room_num = room_num
+
+    def get_person_room(self):
+        return self.room_num
+
+    def get_city_population(self):
+        return self.city_popultaion
+
+
+person = Person(1000000, 10)
+
+print(person.get_person_room())
+print(person.get_city_population())
